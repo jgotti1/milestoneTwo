23fb18e9b3a2d5de26bec4e08c6883e6e98f8477
Jpolo28 <raidenjr28@gmail.com>
2022-07-12 13:15:34 -0400

    schema complete

--- backend/models/citrusProp.js
+++ backend/models/citrusProp.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
-const propertySchema = mongoose.Schema({
+const propertySchema = mongoose.Schema({ 
   property_name: { type: String, required: true },
+  property_address: {type: String, required:true},
+  property_apartment_num: {type:Number, required:true},
+  property_size: {type:Number, required:true},
+  property_rent: {type:Number, required:true},
+  occupied: {type:Boolean, required:true},
+  tenant_name: {type:String, required:false},
+  date_occupied: {type:Date, required:false},
+  num_of_occupants: {type:Number, required:false},
+  pets: {type:Boolean, required:true},
+  late_payment: {type:Boolean, required:false},
+  tenant_comments: {type:String, required:false}
 });
 
 const CitrusProperty = mongoose.model("CitrusProperty", propertySchema);
