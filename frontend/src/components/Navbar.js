bb05b13f02887c15f68a822e336d3f40ecda7a7b
DavidWendtNJIT <djw5383@gmail.com>
2022-07-13 19:23:14 -0400

    dfasdfsdf

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-// import { Link } from "react-router-dom";
-
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-
-const Navbar = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Citrus Property Management
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
-
-export default Navbar;
-=======
 import { Link } from 'react-router-dom'
 
 import AppBar from '@mui/material/AppBar';
@@ -70,4 +33,3 @@ const Navbar = () => {
 }
 
 export default Navbar;
->>>>>>> 023048220222a019de4ba4d2c0fdd70593c36f5b
