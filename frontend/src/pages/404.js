63d12065a1c22e689248eab53339e87fd53818a1
DavidWendtNJIT <djw5383@gmail.com>
2022-07-16 11:26:17 -0400

    Nav and Footer margins updated. 404 Page done.

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -3,15 +3,15 @@ import { Paper, Typography, Button, Box } from "@mui/material"
 const FourOhFour = () => {
   return (
     <Box textAlign="center">
-      <Paper variant="outlined">
         <img src="../images/orange-slice.png" />
-        <Typography variant="h2">
+        <Typography 
+          variant="h2"
+          sx={{mb:3}}>
           404 Error. Page Not Found
         </Typography>
-        <Button variant="contained" href="/Home">
+        <Button variant="contained" href="/">
           Go Back Home
         </Button>
-      </Paper>
     </Box>
   );
 }
