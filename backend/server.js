7348cfb061861ebc904146e58b9fe1f8ccb35da2
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-07-16 18:37:40 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 const path = require("path");
 const router = require("./routes/citrusProp.js");
 
@@ -26,10 +25,8 @@ app.get("/api/citrus", (req, res) => {
   console.log(res);
   res.send("Hello Citrus Property Managment Site!");
 });
-const PORT = 5000;
-MOSGO_URI = "mongodb+srv://citrusProp:citrusProp321123@cirrusprop.7jtgi.mongodb.net/?retryWrites=true&w=majority";
+
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  // .connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Mongo connection is established and running on port: ${PORT} `)))
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(process.env.PORT, () => console.log(`Mongo connection is established and running on port: ${process.env.PORT} `)))
   .catch((err) => console.log(err.message));
