76562a8cc296f6ae4cc71279dddbd0413f35d832
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-07-17 19:15:05 -0400

    updated file structure and home page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 import AddNewProp from "./pages/addNewProp";
-<<<<<<< HEAD:frontend/src/App.js
-import ShowProps from "./pages/ShowProps";
-=======
 import ShowProps from "./pages/showProp";
->>>>>>> 0449b38165641cb0c5181f0a4aaa449a18ef33e5:src/App.js
 import FourOhFour from "./pages/404";
 
 function App() {
@@ -21,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/AddNewProp" element={<AddNewProp />} />
-            <Route path="/ShowProp" element={<ShowProps />}></Route>
+            <Route path="/showProp" element={<ShowProps />}></Route>
             {/* <Route path="*" element={<FourOhFour />}></Route> */}
           </Routes>
         </div>
